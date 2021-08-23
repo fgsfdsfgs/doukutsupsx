65cb37c1724cdf57f09830c56f13a4a6f14adfe0
fgsfds <pvt.fgsfds@gmail.com>
2021-08-24 01:37:44 +0300

    remove mute before address change, volume takes some time to apply on real hw

--- src/engine/spu.c
+++ src/engine/spu.c
@@ -132,16 +132,16 @@ void spu_flush_voices(void) {
 }
 
 void spu_play_sample(const u32 ch, const u32 addr, const u32 freq) {
-  // quiet down to ease transfer
-  SPU_VOICE(ch)->vol_left = 0;
-  SPU_VOICE(ch)->vol_right = 0;
   voice_state[ch].freq = freq_to_pitch(freq);
   voice_state[ch].addr = (addr >> 3);
   SPU_VOICE(ch)->sample_rate = voice_state[ch].freq;
   SPU_VOICE(ch)->sample_startaddr = voice_state[ch].addr;
   spu_key_on(SPU_VOICECH(ch)); // this restarts the channel on the new address
-  spu_update_voice_volume(ch); // restore volume
-  voice_state[ch].dirty = 0; // make sure next flush call doesn't touch it
+  if (voice_state[ch].dirty) {
+     // update volume in case it was changed
+    spu_update_voice_volume(ch);
+    voice_state[ch].dirty = 0;
+  }
 }
 
 void spu_wait_for_transfer(void) {
